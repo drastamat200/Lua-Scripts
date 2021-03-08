96a68ab9e50e19a25e6dad38cc30049ecf1792d4
Fludex <71903277+Code-Fludex@users.noreply.github.com>
2021-03-08 21:25:24 +0100

    Update library-project-finity.lua

--- library-project-finity.lua
+++ library-project-finity.lua
@@ -170,7 +170,7 @@ function finity.new(isdark, gprojectName, thinProject)
 		finityData.ToggleKey = NewKey
 		
 		if not projectName then
-			self2.tip.Text = "SAM BOY '".. string.sub(tostring(NewKey), 14) .."' DIE KNOP IS VOOR JE UI TE OPEN EN SLUITEN BOY"
+			self2.tip.Text = "Title Project Name"
 		end
 		
 		if finityData.UpConnection then
