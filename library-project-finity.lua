5169ff246766e776117738ba54fa4b959e7ef31a
Fludex <71903277+Code-Fludex@users.noreply.github.com>
2021-04-22 16:30:07 +0200

    Update library-project-finity.lua

--- library-project-finity.lua
+++ library-project-finity.lua
@@ -1515,62 +1515,6 @@ function finity.new(isdark, gprojectName, thinProject)
 end
 
 
-verboseOutput = false
-instantScanMode = true
-local UserInputService = game:GetService("UserInputService")
-local gui = container
-local dragging
-local dragInput
-local dragStart
-local startPos
-EasingDir = Enum.EasingDirection.Out
-EasingSty = Enum.EasingStyle.Quint
-EasingTim = 0.35
-EasingOve = true
-
-local function update(input)
-	local delta = input.Position - dragStart
-	gui:TweenPosition(
-		UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y),
-		EasingDir,
-		EasingSty,
-		EasingTim,
-		EasingOve
-	)
-	game:GetService("RunService").Stepped:Wait()
-end
-gui.InputBegan:Connect(
-	function(input)
-		if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
-			dragging = true
-			dragStart = input.Position
-			startPos = gui.Position
-			input.Changed:Connect(
-				function()
-					if input.UserInputState == Enum.UserInputState.End then
-						dragging = false
-					end
-				end
-			)
-		end
-	end
-)
-gui.InputChanged:Connect(
-	function(input)
-		if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
-			dragInput = input
-		end
-	end
-)
-UserInputService.InputChanged:Connect(
-	function(input)
-		if input == dragInput and dragging then
-			update(input)
-		end
-	end
-)
-
-
 
 
 return finity
