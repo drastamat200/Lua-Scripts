6c2431e6253ef944727413941960ef6a1a4b0f48
Fludex <71903277+Code-Fludex@users.noreply.github.com>
2021-04-25 20:47:55 +0200

    Update library-project-finity.lua

--- library-project-finity.lua
+++ library-project-finity.lua
@@ -50,7 +50,7 @@ finity.theme = { -- light
 
 finity.dark_theme = { -- dark
 	main_container = Color3.fromRGB(32, 32, 33),
-	separator_color = Color3.fromRGB(70, 70, 70),
+	separator_color = Color3.fromRGB(55, 55, 55),
 
 	text_color = Color3.fromRGB(206, 206, 206),
 
